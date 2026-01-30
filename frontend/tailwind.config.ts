c25fce72d9308a5244dec078509e369d9c5dfc08
sangphamtq <sangpv8@gmail.com>
2026-01-30 14:45:42 +0700

    setup frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,27 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // primary: {
+        //   DEFAULT: "#148914",
+        //   100: "#E6F4E6",
+        //   200: "#C3E3C3",
+        //   300: "#9ED19E",
+        //   400: "#5FB05F",
+        //   500: "#2F962F",
+        //   600: "#148914",
+        //   700: "#0F6F0F",
+        //   800: "#0B550B",
+        //   900: "#063A06",
+        //   hover: "#0F6F0F"
+        // }
+        primary: {
+          DEFAULT: '#9333ea',
+          100: '#f3e8ff',
+          hover: '#7e22ce'
+        }
+      },
+    },
+  },
+}
